c93fd37bfc2a169f334bdeaee1d3c3250071cdf8
gengbin <1371103410@qq.com>
2023-09-05 14:59:34 +0800

    update explanatory note and params relative path

--- src/logPath.js
+++ src/logPath.js
@@ -1,15 +1,23 @@
 const path =  require("path");
 const fs = require("fs");
 
+/**
+ * For Markdown link to path
+ * @param sqlRequireFolderPath
+ * @param sqlFileArr
+ * @param justRelativePath
+ * @returns {string}
+ */
 function outputFilePath({
   sqlRequireFolderPath,
   sqlFileArr=[],
+  justRelativePath='',
 }){
   return sqlFileArr.map((item) => {
     const allPath = path.join(sqlRequireFolderPath, item)
     const text = fs.readFileSync(allPath, 'utf8')
     const cn_title = text.split(/\r?\n/)[0].replace('-- ', '')
-    return `[${cn_title} ${item}](./src/sql/${item})`
+    return `[${cn_title} ${item}](${justRelativePath}${item})`
   }).reduce((total, item, index, arr) => {
     total += item
     if (arr.length - 1 >= index) total += '<br>\n'
