ae562e167b72528a15849c6577bcc02f1ec93323
GengBin <1371103410@qq.com>
2023-09-17 14:42:02 +0800

    add multiple type check

--- src/sqlColumn.js
+++ src/sqlColumn.js
@@ -13,6 +13,13 @@ exports.makeInitInsert = ({
     if(index==0) str+=`INSERT INTO ${tableName}(${columnArr.map(i=>`\`${i}\``).join(',')}) VALUES`
     str += item.reduce((total, item, index,arr )=>{
       if(index===0) total += `(`
+      const checkArr = [
+        [ '[object Number]', item ],
+        [ '[object Null]', 'null' ],
+        [ '[object String]', `'${item.replace(/\'/g,'\\\'')}'` ],
+      ]
+      const result = checkArr.find(i=>toString.call(item)===i[0])
+      total += result?result[1]:'null'
       total += toString.call(item)==='[object Number]'?item:`'${item.replace(/\'/g,'\\\'')}'`
       total += arr.length -1 ===index?')':','
       return total
