13010a047404b991c04bdf07dde2c41b5331c44d
gengbin <1371103410@qq.com>
2023-09-22 15:21:44 +0800

    add views build

--- src/core.js
+++ src/core.js
@@ -7,8 +7,16 @@ function getSqlText({ allPath='' }){
   if(!allPath && !fs.existsSync(allPath)) return ''
   return fs.readFileSync(allPath,'utf8')
 }
+function viewSqlWrite({
+  viewFolderPath='',
+}) {
+  if(!viewFolderPath) return []
+  const arr = fs.readdirSync(path.join(viewFolderPath))
+  return arr.filter(i=>path.extname(i).includes('sql'))
+}
 exports.build = ({
   sqlFileFolderPath = '',
+  viewFolderPath='',
   buildDist = '',
 }) => {
   if(!sqlFileFolderPath || !path.join(sqlFileFolderPath)) return console.log('sqlFileFolderPath is error')
@@ -20,6 +28,15 @@ exports.build = ({
     writeAllFile({
       distPath: writeToFile,
       text: getSqlText({allPath: path.join(sqlFileFolderPath,item) })+'\n'
+    })
+    console.log(`${item} done`)
+  })
+  viewSqlWrite({
+    viewFolderPath,
+  }).forEach(item=>{
+    writeAllFile({
+      distPath: writeToFile,
+      text: getSqlText({allPath: path.join(viewFolderPath,item) })+'\n'
     })
     console.log(`${item} done`)
   })
